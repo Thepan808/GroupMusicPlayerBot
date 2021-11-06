8c68d3af44a174d21f2c00160aaa4c0181347fad
ঔৣ͜͡➳ 亇ቾ❍ ✟ア入れ刃入✟ <89053669+Thepan808@users.noreply.github.com>
2021-11-06 07:07:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pyrogram import Client as Bot
 
 from callsmusic import run
 from config import API_HASH, API_ID, BOT_TOKEN
-from handlers import __version__
+
 
 bot = Bot(
     ":memory:",
@@ -12,8 +12,6 @@ bot = Bot(
     bot_token=BOT_TOKEN,
     plugins=dict(root="handlers"),
 )
-
-print(f"[INFO]: GRAVE SAD v{__version__} STARTED !")
 
 bot.start()
 run()
